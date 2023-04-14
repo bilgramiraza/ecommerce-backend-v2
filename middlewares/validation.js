0a5475137b39f951a09b2a90389d5e69d165a14a
bilgramiraza <bilgramiraza@yahoo.com>
2023-04-14 16:49:20 +0530

    Added Validation and ErrorHandling for Multer inputs

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,4 +1,5 @@
-const {body, validationResult} = require('express-validator');
+const {body, check, validationResult} = require('express-validator');
+const multerMiddleware = require('./multerMiddleware');
 
 function validationObject(req,res,next){
   //Creates an ErrorHandling Object in the format of 
@@ -9,6 +10,47 @@ function validationObject(req,res,next){
   return next();
 }
 
+const multerValidation = (inputField, minCount)=>{
+  return check(inputField).custom((value, {req})=>{
+    const files = req?.files[inputField];
+    if(!files || !files.length){ 
+      throw new Error(`No ${inputField} uploaded. Only PNG/JPG/JPEG images Allowed.`);
+    }
+    if(files.length < minCount){
+      throw new Error(`Please Upload atleast ${minCount} ${inputField}.`);
+    }
+    return true;
+  })
+}
+
+async function multerCleanup(req,res,next) {
+  try{
+    if (Object.keys(req?.errorObject).length>0 && req?.files) {
+      const promises = [];
+      const { productImage, descriptionImages } = req.files;
+      if (productImage) {
+        promises.push(unlink(productImage[0].path));
+      }
+      if (descriptionImages) {
+        if(Array.isArray(descriptionImages)){
+          promises.push(
+            ...descriptionImages.map((file) => {
+              return unlink(file.path);
+            })
+          );
+        }else{
+          promises.push(unlink(descriptionImages.path));
+        }
+      }
+      await Promise.all(promises);
+      req.errorObject.img = true;
+    }
+    return next();
+  }catch(err){
+    return next(err);
+  }
+};
+
 exports.productValidation = [
   body('name','Product Name Cannot be Empty').trim().isLength({min:1}).escape(),
   body('description','Product Description Cannot be Empty').trim().isLength({min:1}).escape(),
@@ -16,7 +58,14 @@ exports.productValidation = [
   body('category','Product Category Must be Selected').trim().isLength({min:1}).escape(),
   body('quantity','Product Quantity cannot be Empty').isInt({gt:0}).withMessage('Product Quantity must be Positive'),
   body('price','Product Price cannot be Empty').isFloat({gt:0}).withMessage('Product Price must be Positive'),
+  multerMiddleware.fields([
+    { name: 'productImage', maxCount:1},
+    { name: 'descriptionImages', maxCount:5},
+  ]),
+  multerValidation('productImage',1),
+  multerValidation('descriptionImages',2),
   validationObject,
+  multerCleanup,
 ];
 
 exports.categoryValidation = [
